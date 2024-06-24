cb432b7e9a8684b101dbfd97de365554b1e2c96d
Samrat Bose <samratofficial2009@gmail.com>
2024-06-24 15:33:28 +0530

    done some changes on css and js

--- script.js
+++ script.js
@@ -1,11 +1,47 @@
-async function generateCompliment() {
-    try {
-        const response = await fetch('https://complimentr.com/api');
-        const data = await response.json();
-        const compliment = data.compliment;
-        document.getElementById('compliment').textContent = compliment;
-    } catch (error) {
-        console.error('Error fetching the compliment:', error);
-        document.getElementById('compliment').textContent = 'Oops! Something went wrong. Try again.';
-    }
+const compliments = [
+    "You're an awesome friend.",
+    "You're a gift to those around you.",
+    "You're a smart cookie.",
+    "You are awesome!",
+    "You have impeccable manners.",
+    "I like your style.",
+    "You have the best laugh.",
+    "I appreciate you.",
+    "You are the most perfect you there is.",
+    "You are enough.",
+    "You're strong.",
+    "Your perspective is refreshing.",
+    "You're an incredible human being.",
+    "You're a true gift to the people in your life.",
+    "You're amazing just the way you are.",
+    "You bring out the best in other people.",
+    "You're a ray of sunshine on a rainy day.",
+    "You have a great sense of humor.",
+    "You're more helpful than you realize.",
+    "You're a great listener."
+];
+
+function generateCompliment() {
+    const complimentElement = document.getElementById('compliment');
+    const randomIndex = Math.floor(Math.random() * compliments.length);
+    const compliment = compliments[randomIndex];
+    complimentElement.textContent = compliment;
 }
+
+function enableAudio() {
+    const audio = document.getElementById('background-music');
+    audio.play();
+    document.getElementById('audio-popup').style.display = 'none';
+}
+
+$(document).on('click', '.toggle-sound', function(e) {
+    const audio = document.getElementById('background-music');
+    $(this).toggleClass('sound-mute');
+    if (audio.muted) {
+        audio.muted = false;
+        $('.sound--icon').removeClass('fa-volume-mute').addClass('fa-volume-up');
+    } else {
+        audio.muted = true;
+        $('.sound--icon').removeClass('fa-volume-up').addClass('fa-volume-mute');
+    }
+});
